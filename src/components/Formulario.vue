e9208da6209131221382a5816791f85540c004d5
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-12-09 17:12:29 -0300

    First commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="box formulario">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Fomulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao" />                            
+            </div>
+            <div class="column">
+                <TemporizadorTarefa @aoTemporizadorFinalizado="finalizarTarefa"/>
+            </div>
+        </div>                
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TemporizadorTarefa from './Temporizador.vue'
+
+export default defineComponent({
+    name: 'FormularioTarefa',
+    emits: ['aoSalvarTarefa'],
+    components: {
+        TemporizadorTarefa
+    },
+    data(){
+        return {
+            descricao: ''
+        }
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number) : void{
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao 
+            })
+            
+            this.descricao = ''
+        }
+    }
+})
+</script>
+
+<style>
+.formulario {
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+}
+</style>
