b62df30c328ac7ccf67c774b185d148486163c17
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-12-26 11:36:06 -0300

    Refactored Formulario view added lifecycle hooks

--- README.md
+++ README.md
@@ -1,24 +1,43 @@
 # alura-tracker
 
+## Requirements
+
+install json-server:
+
+```
+npm install json-server
+```
+
+start server:
+
+```
+json-server --watch db.json
+```
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,83 +1,83 @@
 <template>
-    <section>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">
-                    Nome do Projeto
-                </label>
-                <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjet" />                 
-            </div>
+  <section>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjet" />
+      </div>
 
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>        
-    </section>
-    
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script lang="ts">
-import { useStore } from '@/store'
-import { defineComponent } from 'vue'
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo_mutacoes'
-import { TipoNotificacao } from '@/interfaces/INotificacao'
-import useNotificador from '@/hooks/notificador'
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from '@/store/tipo_acoes'
+import { useStore } from "@/store";
+import { defineComponent, ref } from "vue";
+import { TipoNotificacao } from "@/interfaces/INotificacao";
+import useNotificador from "@/hooks/notificador";
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from "@/store/tipo_acoes";
 
 export default defineComponent({
-    name: 'FormularioProjeto',
-    props:{
-        id:{
-            type: String
-        }
+  name: "FormularioProjeto",
+  props: {
+    id: {
+      type: String,
     },
-    mounted() {
-        if(this.id)
-        {
-            const projeto = this.store.state.projeto.projetos.find(proj => proj.id == this.id)
-            this.nomeDoProjeto = projeto?.nome || ''
-        }
+  },
+  // data() {
+  //   return {
+  //     nomeDoProjeto: "",
+  //   };
+  // },
+  methods: {
+    salvar() {
+      if (this.id) {
+        this.store
+          .dispatch(ALTERAR_PROJETO, {
+            id: this.id,
+            nome: this.nomeDoProjeto,
+          })
+          .then(() => {
+            this.lidarComSucesso();
+          });
+      } else {
+        this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto).then(() => {
+          this.lidarComSucesso();
+        });
+      }
     },
-    data (){
-        return {
-            nomeDoProjeto: '',
-        }
+    lidarComSucesso() {
+      this.nomeDoProjeto = "";
+      this.notificar(
+        TipoNotificacao.SUCESSO,
+        "Novo projeto foi salvo",
+        "Prontinho! Seu projeto já está disponível."
+      );
+      this.$router.push("/projetos");
     },
-    methods:{
-        salvar(){
-            if(this.id)
-            {
-                this.store.dispatch(ALTERAR_PROJETO, {
-                    id: this.id,
-                    nome: this.nomeDoProjeto
-                }).then(() => {
-                    this.lidarComSucesso()
-                })
-            }
-            else{
-                this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
-                    .then(() => {
-                       this.lidarComSucesso()
-                    })
-            }
-        },
-        lidarComSucesso(){
-            this.nomeDoProjeto = ''
-            this.notificar(TipoNotificacao.SUCESSO,  'Novo projeto foi salvo', 'Prontinho! Seu projeto já está disponível.')
-            this.$router.push('/projetos')
-        }
-        
-    },
-    setup() {
-        const store = useStore()
-        const { notificar } = useNotificador()
+  },
+  setup(props) {
+    const store = useStore();
+    const { notificar } = useNotificador();
 
-        return {
-            store,
-            notificar
-        }
+    const nomeDoProjeto = ref("")
+
+    if (props.id) {
+      const projeto = store.state.projeto.projetos.find(
+        (proj) => proj.id == props.id
+      );
+
+      nomeDoProjeto.value = projeto?.nome || "";
     }
-})
+    
+    return {
+      store,
+      notificar,
+      nomeDoProjeto
+    };
+  },
+});
 </script>
